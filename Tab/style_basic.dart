285e77d559b57264e4791abefa3eed56380237eb
beakyangsu <qordidtn02@gmail.com>
2021-12-15 13:45:28 +0900

    Create style_basic.dart

---
+++ Tab/style_basic.dart
@@ -0,0 +1,61 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyDemo(),
+    );
+  }
+}
+
+class MyDemo extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            bottom: TabBar(indicatorSize: TabBarIndicatorSize.label, tabs: [
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text("APPS"),
+                ),
+              ),
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text("MOVIES"),
+                ),
+              ),
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text("GAMES"),
+                ),
+              ),
+            ]),
+          ),
+          body: TabBarView(children: [
+            Icon(Icons.apps),
+            Icon(Icons.movie),
+            Icon(Icons.games),
+          ]),
+        )
+    );
+  }
+}
