c3d918c25e32cf4612bea06d653261209a9e3167
beakyangsu <qordidtn02@gmail.com>
2021-12-21 15:46:43 +0900

    Add files via upload

---
+++ gradient/app/src/main/java/com/example/disney_plus_compare_android/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.disney_plus_compare_android
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
